5beb404049a65c67c35c730d514d75d014037eb0
Marco.Biundo <marcojrdepew@gmail.com>
2020-04-05 13:52:18 -0400

    Changes to text blurbs

--- src/components/DTIdescription.vue
+++ src/components/DTIdescription.vue
@@ -35,15 +35,9 @@ export default {
   },
   data() {
     return {
-      title: "Description",
+      title: "Defining the Intervention",
       text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint \
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt \
-        mollit anim id est laborum.",
+        "The “intervention” includes: \n1) what you are implementing -  the practice, procedure, policy or program, and: \n2) how you are implementing -  the strategies you are using to implement that practice, procedure, policy or program. ",
       links: [
         { link: "www.google.com", display: "Google 1" },
         { link: "www.google.com", display: "Google 2" },
